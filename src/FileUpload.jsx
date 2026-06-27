37d948db379a7662aab25c4b1744710f7761ccc1
Shurzong Lo <shurzonglo@gmail.com>
2026-06-27 15:23:53 -0400

    fileread works yay

--- src/FileUpload.jsx
+++ src/FileUpload.jsx
@@ -1,20 +1,30 @@
 // SO APPARENTLY FILE SYSTEM ACCESS API DOESN'T WORK ON FIREFOX
 // [https://developer.chrome.com/docs/capabilities/web-apis/file-system-access] <- REASON
+
 function FileUpload() {
-    async function fileClick() {
-        console.log("'as been clicked");
-        const [fileHandle] = await window.showOpenFilePicker();
-        const file = fileHandle.getFile(); // file is of File
-        console.log((await file).name); 
-    }
+    // async function fileClick() {
+    //     console.log("'as been clicked");
+    //     const [fileHandle] = await window.showOpenFilePicker();
+    //     const file = fileHandle.getFile(); // file is of File
+    //     console.log((await file).name); 
+    // }
+
+    async function fileChange(event) {
+        const fileList = event.target.files;
+        const audio = document.getElementById("audio_player");
+        audio.src = URL.createObjectURL(fileList[0]);
+        audio.play();
+        
+    };
 
     return (
     <>
-        <button onClick={fileClick}>
-            buttonson
-        </button>
+        <input type="file" id="file-selector" accept=".mp3" onChange={fileChange} multiple/>
+        <audio id="audio_player"/>
     </>
     );
 }
+
+
 
 export default FileUpload;
